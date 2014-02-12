ac83d4eae32fe57f575c11b079f34a2a5b5f28ee
Jason Bock <jason@jasonbock.net>
2014-02-12 10:31:59 -0600

    Got a TS version of trivia fetch working

--- RushIsAwesome/Scripts/Trivia.ts
+++ RushIsAwesome/Scripts/Trivia.ts
@@ -1,22 +1,23 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
-var Trivia = (function () {
-    function Trivia() {
+
+class Trivia {
+    triviaElement: JQuery;
+
+    constructor() {
         this.triviaElement = $('#trivia');
     }
-    Trivia.prototype.retrieve = function () {
+    retrieve() {
         var self = this;
         $.getJSON('api/trivia', function (data, textStatus, jqXHR) {
             self.triviaElement.text(data.Item);
         });
-    };
-    return Trivia;
-})();
+    }
+}
 
-window.onload = function () {
+window.onload = () => {
     var trivia = new Trivia();
-    var didYouKnowButton = document.getElementById('didYouKnow');
+    var didYouKnowButton = <HTMLButtonElement>document.getElementById('didYouKnow');
     didYouKnowButton.onclick = function () {
         trivia.retrieve();
     };
-};
-//# sourceMappingURL=Trivia.js.map
+}

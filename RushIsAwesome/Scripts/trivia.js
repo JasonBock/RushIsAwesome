fd45db69257f3ab9b2de52e0bcb4b8cdbfb9ce37
Jason Bock <jason@jasonbock.net>
2014-02-12 08:43:34 -0600

    Added trivia to main page

---
+++ RushIsAwesome/Scripts/trivia.js
@@ -0,0 +1,11 @@
+﻿var uri = 'api/products';
+
+function loadTrivia() {
+	$.getJSON('api/trivia')
+		 .done(function (data) {
+		 	$('#trivia').text(data.Item);
+		 })
+		 .fail(function (jqXHR, textStatus, err) {
+		 	$('#trivia').text('Could not load trivia.');
+		 });
+}
